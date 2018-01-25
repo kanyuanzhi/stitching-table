1a2eb980134a100e56ef65625578d01fa62790dd
kanyuanzhi <kan@mail.ustc.edu.cn>
2018-01-25 16:48:32 +0800

    finish

--- main.py
+++ main.py
@@ -1,31 +1,60 @@
+# -*- coding: UTF-8 -*-
 import xlrd
 import xlwt
+import getpass
 import os
 
-xlrd.Book.encoding = "utf8"
-test = xlrd.open_workbook('test.xlsx')
-table = test.sheet_by_index(0)
-
-print table.nrows
-print table.ncols
-print test.nsheets
-
-fileNames = os.listdir('./')
+user = getpass.getuser()
+tablePath = 'C:/Users/' + user + '/Desktop/tables/'
+fileNames = os.listdir(tablePath)
 tableNames = []
 
 for name in fileNames:
-    #if name.endswith('.xls') or name.endswith('.xlsx'):
-    if name.endswith('.xls'):
+    if name.endswith('.xls') or name.endswith('.xlsx'):
+        # if name.endswith('.xls'):
         tableNames.append(name)
 
-print fileNames
-print tableNames
-
 nameToTable = dict.fromkeys(tableNames)
+nameToMatrixProcessed = dict.fromkeys(tableNames)  # 处理后表格，添加项目代码与项目名称字段，删除前两行表头
 
 for name in tableNames:
-    nameToTable[name] = xlrd.open_workbook(name).sheet_by_index(0)
+    nameToTable[name] = xlrd.open_workbook(tablePath + name).sheet_by_index(0)
+    cols_count = nameToTable[name].ncols
+    rows_count = nameToTable[name].nrows
+    nameToMatrixProcessed[name] = [["" for i in range(cols_count)] for j in range(rows_count - 2)]
+    projectCode = nameToTable[name].cell(0, 1).value  # 项目代码
+    projectName = nameToTable[name].cell(0, 3).value  # 项目名称
+    for i in range(rows_count - 2):
+        for j in range(cols_count):
+            nameToMatrixProcessed[name][i][j] = nameToTable[name].cell(i + 2, j).value
+        nameToMatrixProcessed[name][i].append(projectCode)
+        nameToMatrixProcessed[name][i].append(projectName)
 
-print nameToTable
+##################################################################
+# 处理表头
+tableHead = []
+flag = False
+for th in nameToTable[tableNames[0]].row_values(1):
+    if th == u"摘要":
+        summaryIndex = nameToTable[tableNames[0]].row_values(1).index(th)
+        flag = True
+    tableHead.append(th)
+tableHead.append(u"项目代码")
+tableHead.append(u"项目名称")
+if flag:
+    tableHead.append(u"姓名")
+##################################################################
 
-print nameToTable['CX2100060032.xls'].row_values(0)
+finalMatrix = [tableHead]
+for value in nameToMatrixProcessed.itervalues():
+    if flag:
+        for item in value:
+            item.append(item[summaryIndex][0:3])
+    finalMatrix.extend(value)
+
+file = xlwt.Workbook()
+table = file.add_sheet('ceshi', cell_overwrite_ok=True)
+for i in range(len(finalMatrix)):
+    for j in range(len(finalMatrix[0])):
+        table.write(i, j, finalMatrix[i][j])
+file.save('result.xls')
