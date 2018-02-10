d4bc316d386571a64d30bbfe764835c3fe18b2b5
kanyuanzhi <kan@mail.ustc.edu.cn>
2018-02-10 13:23:56 +0800

    add exchange

--- merge.py
+++ merge.py
@@ -5,15 +5,26 @@ import getpass
 import os
 
 user = getpass.getuser()
-tablePath = 'C:/Users/' + user + '/Desktop/two_tables/'
-# tablePath = 'two_tables/'
+# tablePath = 'C:/Users/' + user + '/Desktop/two_tables/'
+tablePath = 'two_tables/'
 fileNames = os.listdir(tablePath)
 
 nameToTable = []
 nameToMatrix = []
 
-nameToTable.append(xlrd.open_workbook(tablePath + 'mingxi.xls').sheet_by_index(0))
-nameToTable.append(xlrd.open_workbook(tablePath + 'yusuan.xls').sheet_by_index(0))
+nameToTable.append(xlrd.open_workbook(tablePath + '明细.xls').sheet_by_index(0))
+nameToTable.append(xlrd.open_workbook(tablePath + '预算.xls').sheet_by_index(0))
+nameToTable.append(xlrd.open_workbook(tablePath + '分项代码转换表.xls').sheet_by_index(0))
+
+##############处理分项代码转换表###################
+
+codeChangeTable = []
+
+
+
+
+
+################################
 
 for m in range(2):
     cols_count = nameToTable[m].ncols
@@ -37,9 +48,35 @@ for i in range(len(nameToMatrix[0])):
     if len(nameToMatrix[0][i]) == 8:
         nameToMatrix[0][i].extend(["-", "-", "-", "-", "-", "-", "-", "-"])
 
+tempMatrix = []
+for j in range(len(nameToMatrix[1])):
+    projectCode1 = nameToMatrix[1][j][6]
+    itemCode1 = nameToMatrix[1][j][0]
+    flagProject = False
+    flagItem = False
+    for i in range(len(nameToMatrix[0])):
+        projectCode0 = nameToMatrix[0][i][8]
+        itemCode0 = nameToMatrix[0][i][0]
+        if projectCode1 == projectCode0:
+            flagProject = True
+            if itemCode1 == itemCode0:
+                flagItem = True
+    if not flagProject:  # 处理预算中有该分项代码但明细中没有
+        temp = ["-", nameToMatrix[1][j][1], "-", "-", "-", "-", "-", "-", projectCode1, "-", "-"]
+        temp.extend(nameToMatrix[1][j])
+        tempMatrix.append(temp)
+    else:
+        if not flagItem:  # 处理预算中有该分项名称但明细中没有
+            temp = ["-", nameToMatrix[1][j][1], "-", "-", "-", "-", "-", "-", projectCode1, "-", "-"]
+            temp.extend(nameToMatrix[1][j])
+            tempMatrix.append(temp)
+nameToMatrix[0].extend(tempMatrix)
+
 finalTable = [[] for j in range(len(nameToMatrix[0]) + 1)]
-finalTable[0].append(u"分项代码")
+finalTable[0].append(u"项目代码")
 finalTable[0].append(u"分项名称")
+finalTable[0].append(u"原分项代码")
+finalTable[0].append(u"新分项代码")
 finalTable[0].append(u"预算数")
 finalTable[0].append(u"历年累计支出(不含借款)")
 finalTable[0].append(u"余额")
@@ -50,8 +87,10 @@ finalTable[0].append(u"金额")
 finalTable[0].append(u"报销金额占总支出比")
 
 for i in range(len(nameToMatrix[0])):
-    finalTable[i + 1].append(nameToMatrix[0][i][8])  # 分项代码
+    finalTable[i + 1].append(nameToMatrix[0][i][8])  # 项目代码
     finalTable[i + 1].append(nameToMatrix[0][i][1])  # 分项名称
+    finalTable[i + 1].append(nameToMatrix[0][i][0])  # 原分项代码
+    finalTable[i + 1].append('new_xxx')  # 新分项代码
     finalTable[i + 1].append(nameToMatrix[0][i][13])  # 预算数
     finalTable[i + 1].append(nameToMatrix[0][i][15])  # 历年累计支出(不含借款)
     finalTable[i + 1].append(nameToMatrix[0][i][16])  # 余额
